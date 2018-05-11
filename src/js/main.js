ae322e1c5bf6df7263ac0c133cb56b1fb744123f
MrHidalgo <Luckyhidalgo@gmail.com>
2018-05-11 18:17:07 +0300

    Slide 5 - replace from flex to inline-block. Modal bg - add animation. Some change and fix section structure.

--- src/js/main.js
+++ src/js/main.js
@@ -7,44 +7,16 @@ $(document).ready(function () {
   var _window = $(window);
   var _document = $(document);
 
-  // BREAKPOINT SETTINGS
-  var bp = {
-    mobileS: 375,
-    mobile: 568,
-    tablet: 768,
-    desktop: 992,
-    wide: 1336,
-    hd: 1680
-  };
-
-  var easingSwing = [.02, .01, .47, 1]; // default jQuery easing for anime.js
 
   ////////////
   // READY - triggered when PJAX DONE
   ////////////
   function pageReady() {
-    // legacySupport();
-    // updateHeaderActiveClass();
+    legacySupport();
     initHeaderScroll();
-
-    // initSliders();
     initScrollMonitor();
-    // initMasks();
-    // initLazyLoad();
-
-    // development helper
-    // _window.on('resize', debounce(setBreakpoint, 200))
-
-    // AVAILABLE in _components folder
-    // copy paste in main.js and initialize here
-
-    // initTeleport();
-    // parseSvg();
-    // revealFooter();
-    // _window.on('resize', throttle(revealFooter, 100));
   }
 
-  // this is a master function which should have all functionality
   pageReady();
 
 
@@ -81,17 +53,17 @@ $(document).ready(function () {
   // COMMON
   //////////
 
-  // function legacySupport(){
-  //   // svg support for laggy browsers
-  //   svg4everybody();
-  //
-  //   // Viewport units buggyfill
-  //   window.viewportUnitsBuggyfill.init({
-  //     force: true,
-  //     refreshDebounceWait: 150,
-  //     appendToBody: true
-  //   });
-  // }
+  function legacySupport(){
+    // svg support for laggy browsers
+    svg4everybody();
+
+    // Viewport units buggyfill
+    window.viewportUnitsBuggyfill.init({
+      force: true,
+      refreshDebounceWait: 150,
+      appendToBody: true
+    });
+  }
 
 
   // Prevent # behavior
@@ -203,63 +175,10 @@ $(document).ready(function () {
   //   $('.mobile-navi').removeClass('is-active');
   // }
 
-  // SET ACTIVE CLASS IN HEADER
-  // * could be removed in production and server side rendering when header is inside barba-container
-  // function updateHeaderActiveClass(){
-  //   $('.header__menu li').each(function(i,val){
-  //     if ( $(val).find('a').attr('href') == window.location.pathname.split('/').pop() ){
-  //       $(val).addClass('is-active');
-  //     } else {
-  //       $(val).removeClass('is-active')
-  //     }
-  //   });
-  // }
-
-  //////////
-  // SLIDERS
-  //////////
-
-  // function initSliders(){
-  //   var slickNextArrow = '<div class="slick-prev"><svg class="ico ico-back-arrow"><use xlink:href="img/sprite.svg#ico-back-arrow"></use></svg></div>';
-  //   var slickPrevArrow = '<div class="slick-next"><svg class="ico ico-next-arrow"><use xlink:href="img/sprite.svg#ico-next-arrow"></use></svg></div>'
-  //
-  //   // General purpose sliders
-  //   $('[js-slider]').each(function(i, slider){
-  //     var self = $(slider);
-  //
-  //     // set data attributes on slick instance to control
-  //     if (self && self !== undefined) {
-  //       self.slick({
-  //         autoplay: self.data('slick-autoplay') !== undefined ? true : false,
-  //         dots: self.data('slick-dots') !== undefined ? true : false,
-  //         arrows: self.data('slick-arrows') !== undefined ? true : false,
-  //         prevArrow: slickNextArrow,
-  //         nextArrow: slickPrevArrow,
-  //         infinite: self.data('slick-infinite') !== undefined ? true : true,
-  //         speed: 300,
-  //         slidesToShow: 1,
-  //         accessibility: false,
-  //         adaptiveHeight: true,
-  //         draggable: self.data('slick-no-controls') !== undefined ? false : true,
-  //         swipe: self.data('slick-no-controls') !== undefined ? false : true,
-  //         swipeToSlide: self.data('slick-no-controls') !== undefined ? false : true,
-  //         touchMove: self.data('slick-no-controls') !== undefined ? false : true
-  //       });
-  //     }
-  //
-  //   })
-  //
-  //   // other individual sliders goes here
-  //   $('[js-myCustomSlider]').slick({
-  //
-  //   })
-  //
-  // }
 
   //////////
   // MODALS
   //////////
-
   function initPopups(){
     let startWindowScroll = 0;
 
@@ -297,86 +216,43 @@ $(document).ready(function () {
   }
 
   ////////////
-  // UI
-  ////////////
-
-  // textarea autoExpand
-  // _document
-  //   .one('focus.autoExpand', '.ui-group textarea', function(){
-  //       var savedValue = this.value;
-  //       this.value = '';
-  //       this.baseScrollHeight = this.scrollHeight;
-  //       this.value = savedValue;
-  //   })
-  //   .on('input.autoExpand', '.ui-group textarea', function(){
-  //       var minRows = this.getAttribute('data-min-rows')|0, rows;
-  //       this.rows = minRows;
-  //       rows = Math.ceil((this.scrollHeight - this.baseScrollHeight) / 17);
-  //       this.rows = minRows + rows;
-  //   });
-
-  // Masked input
-  // function initMasks(){
-  //   $("[js-dateMask]").mask("99.99.99",{placeholder:"ДД.ММ.ГГ"});
-  //   $("input[type='tel']").mask("+7 (000) 000-0000", {placeholder: "+7 (___) ___-____"});
-  // }
-
-
-  ////////////
   // SCROLLMONITOR - WOW LIKE
   ////////////
   function initScrollMonitor(){
-    $('.wow').each(function(i, el){
+    $(".wow").each(function(i, el){
 
-      var elWatcher = scrollMonitor.create( $(el) );
+      let elWatcher = scrollMonitor.create( $(el) );
 
-      var delay;
+      let delay;
 
-      if ( _window.width() < 768 ){
+      if ( $(window).width() < 768 ){
         delay = 0
       } else {
         delay = $(el).data('animation-delay');
       }
 
-      var animationClass = $(el).data('animation-class') || "wowFadeUp";
-
-      var animationName = $(el).data('animation-name') || "wowFade";
+      let animationClass = $(el).data('animation-class') || "wowFadeUp";
+      let animationName = $(el).data('animation-name') || "wowFade";
 
       elWatcher.enterViewport(throttle(function() {
         $(el).addClass(animationClass);
-        $(el).css({
-          'animation-name': animationName,
-          'animation-delay': delay,
-          'visibility': 'visible'
-        }).addClass("animation-go-js");
-      }, 100, {
-        'leading': true
-      }));
+        $(el)
+          .css(
+            {
+              'animation-name': animationName,
+              'animation-delay': delay,
+              'visibility': 'visible'
+            }
+          )
+          .addClass("animation-go-js");
+      },
+        100,
+        {
+          'leading': true
+        }
+      ));
     });
   }
-
-
-  //////////
-  // LAZY LOAD
-  //////////
-  // function initLazyLoad(){
-  //   _document.find('[js-lazy]').Lazy({
-  //     threshold: 500,
-  //     enableThrottle: true,
-  //     throttle: 100,
-  //     scrollDirection: 'vertical',
-  //     effect: 'fadeIn',
-  //     effectTime: 350,
-  //     // visibleOnly: true,
-  //     // placeholder: "data:image/gif;base64,R0lGODlhEALAPQAPzl5uLr9Nrl8e7...",
-  //     onError: function(element) {
-  //         console.log('error loading ' + element.data('src'));
-  //     },
-  //     beforeLoad: function(element){
-  //       // element.attr('style', '')
-  //     }
-  //   });
-  // }
 
   //////////
   // BARBA PJAX
@@ -458,27 +334,4 @@ $(document).ready(function () {
   //   $(window).scroll();
   //   $(window).resize();
   // }
-
-  //////////
-  // DEVELOPMENT HELPER
-  //////////
-  // function setBreakpoint(){
-  //   var wHost = window.location.host.toLowerCase()
-  //   var displayCondition = wHost.indexOf("localhost") >= 0 || wHost.indexOf("surge") >= 0
-  //   if (displayCondition){
-  //     console.log(displayCondition)
-  //     var wWidth = _window.width();
-  //
-  //     var content = "<div class='dev-bp-debug'>"+wWidth+"</div>";
-  //
-  //     $('.page').append(content);
-  //     setTimeout(function(){
-  //       $('.dev-bp-debug').fadeOut();
-  //     },1000);
-  //     setTimeout(function(){
-  //       $('.dev-bp-debug').remove();
-  //     },1500)
-  //   }
-  // }
-
 });
